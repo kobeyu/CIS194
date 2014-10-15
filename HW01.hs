72d9d7d63835904d886f6419b5bae583243b85fe
Greg Manning <git@app.tockli.me>
2014-10-15 18:16:11 +0100

    HW01 with memoization
    added memoization to the hanoiN function.
    Could probably be tidier, but this is at least *much* faster.

--- HW01.hs
+++ HW01.hs
@@ -3,6 +3,7 @@ import Data.List(unfoldr)
 import Data.Tuple(swap)
 import Data.List(minimumBy)
 import Data.Function(on)
+import Data.Function.Memoize
 
 --LUHN
 
@@ -30,6 +31,10 @@ hanoi 0 _ _ _ = []
 hanoi n a b c = concat [hanoi (n-1) a c b,[(a,b)],hanoi (n-1) c b a]
 
 -- HANOI N
+
+bigT :: Integer -> Int -> Int
+bigT = memoize2 (\d p -> length (hanoiN d [1..p]))
+
 hanoiN :: Integer -> [a] -> [Move a]
 hanoiN 0 _  = []
 hanoiN 1 (a:b:_) = [(a,b)]
@@ -38,8 +43,9 @@ hanoiN n (a:b:c:rest) = concat [ step1 bestK, step2 bestK, step3 bestK]
         step1 k = hanoiN k (a:c:b:rest)
         step2 k = hanoiN (n-k) (a:b:rest)
         step3 k = hanoiN k (c:b:a:rest)
-        bigT k = 2 * length (step1 k) + length (step2 k) 
+        lenRest = length rest
+        len k = 2 * bigT k (lenRest + 3) + bigT (n-k) (lenRest + 2)
         bestK :: Integer
         bestK = if null rest 
             then n - 1 
-            else fromIntegral . minimumBy (compare `on` bigT) $  [1..n-1]
+            else fromIntegral . minimumBy (compare `on` len) $  [1..n-1]
